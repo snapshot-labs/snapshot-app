91e24dba62ec683f20ba5af55a65405b08d744aa
JM Myers <jm.myers@everbridge.com>
2021-09-21 14:41:56 -0400

    Fix package conflicts

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,3 +1,4 @@
 export default {
-    white: "#fff"
-}
+  white: "#fff",
+  black: "#000",
+};
--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Text, View, TouchableOpacity } from "react-native";
+import { HOME_SCREEN } from "../constants/navigation";
+import colors from "../constants/colors";
 
 function LandingScreen() {
+  const navigation = useNavigation();
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, paddingHorizontal: 16 }}>
       <Text style={{ fontFamily: "SpaceMono_700Bold", fontSize: 20 }}>
         snapshot
       </Text>
@@ -18,6 +22,23 @@ function LandingScreen() {
       <Text style={{ fontFamily: "SpaceMono_700Bold", fontSize: 60 }}>
         get made
       </Text>
+
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate(HOME_SCREEN);
+        }}
+      >
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: colors.black,
+            borderRadius: 24,
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: colors.white }}>Log in</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
