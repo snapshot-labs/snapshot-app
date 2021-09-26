00df9e282e67f09af5180271f8b83cc561122096
JM Myers <jm.myers@everbridge.com>
2021-09-25 22:52:57 -0400

    Start adding graphQL queries

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
+import apolloClient from "../util/apolloClient";
+import { FOLLOWS_QUERY } from "../util/queries";
+import { useWalletConnect } from "@walletconnect/react-native-dapp";
+
+async function getFollows(accountId) {
+  const query = {
+    query: FOLLOWS_QUERY,
+    variables: {
+      follower_in: accountId,
+    },
+  };
+  const result = await apolloClient.query(query);
+  let followedSpaces = [];
+  if (result && result.data && result.data.follows) {
+    followedSpaces = result.data.follows.map((follow) => follow.space.id);
+  }
+}
 
 function HomeScreen() {
+  const connector = useWalletConnect();
+
+  useEffect(() => {
+    getFollows(connector.accounts[0]);
+  }, []);
   return <View></View>;
 }
 
---
+++ src/util/queries.ts
@@ -0,0 +1,150 @@
+import gql from "graphql-tag";
+
+export const PROPOSAL_QUERY = gql`
+  query Proposal($id: String!) {
+    proposal(id: $id) {
+      id
+      title
+      body
+      choices
+      start
+      end
+      snapshot
+      state
+      author
+      created
+      plugins
+      network
+      type
+      strategies {
+        name
+        params
+      }
+      space {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const PROPOSALS_QUERY = gql`
+  query Proposals(
+    $first: Int!
+    $skip: Int!
+    $state: String!
+    $space: String
+    $space_in: [String]
+    $author_in: [String]
+  ) {
+    proposals(
+      first: $first
+      skip: $skip
+      where: {
+        space: $space
+        state: $state
+        space_in: $space_in
+        author_in: $author_in
+      }
+    ) {
+      id
+      title
+      body
+      start
+      end
+      state
+      author
+      created
+      space {
+        id
+        name
+        members
+        avatar
+      }
+    }
+  }
+`;
+
+export const PROPOSAL_VOTES_QUERY = gql`
+  query ($id: String!) {
+    proposal(id: $id) {
+      id
+      title
+      body
+      choices
+      start
+      end
+      snapshot
+      state
+      author
+      created
+      plugins
+      network
+      type
+      strategies {
+        name
+        params
+      }
+      space {
+        id
+        name
+      }
+    }
+    votes(first: 10000, where: { proposal: $id }) {
+      id
+      voter
+      created
+      choice
+    }
+  }
+`;
+
+export const FOLLOWS_QUERY = gql`
+  query Follows($space_in: [String], $follower_in: [String]) {
+    follows(where: { space_in: $space_in, follower_in: $follower_in }) {
+      id
+      follower
+      space {
+        id
+      }
+    }
+  }
+`;
+
+export const ALIASES_QUERY = gql`
+  query Aliases($address: String!, $alias: String!) {
+    aliases(where: { address: $address, alias: $alias }) {
+      address
+      alias
+    }
+  }
+`;
+
+export const SPACES_QUERY = gql`
+  query Spaces($id_in: [String]) {
+    spaces(where: { id_in: $id_in }) {
+      id
+      name
+      about
+      network
+      symbol
+      network
+      terms
+      skin
+      avatar
+      twitter
+      github
+      strategies {
+        name
+        params
+      }
+      admins
+      members
+      filters {
+        minScore
+        onlyMembers
+      }
+      plugins
+    }
+  }
+`;
