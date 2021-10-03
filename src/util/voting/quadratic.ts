9bfae93e27aab1c6df05e70831664a6f742a0666
JM Myers <jm.myers@everbridge.com>
2021-10-02 22:41:52 -0400

    Add VotingQuadratic

--- src/util/voting/quadratic.ts
+++ src/util/voting/quadratic.ts
@@ -1,6 +1,7 @@
 export function percentageOfTotal(i, values, total) {
   const reducedTotal: any = total.reduce((a: any, b: any) => a + b, 0);
   const percent = (values[i] / reducedTotal) * 100;
+
   return isNaN(percent) ? 0 : percent;
 }
 
@@ -27,14 +28,14 @@ export default class ApprovalVoting {
     const results = this.proposal.choices
       .map((choice, i) =>
         this.votes
-          .map(vote => quadraticMath(i, vote.choice, vote.balance))
+          .map((vote) => quadraticMath(i, vote.choice, vote.balance))
           .reduce((a, b: any) => a + b, 0)
       )
-      .map(sqrt => sqrt * sqrt);
+      .map((sqrt) => sqrt * sqrt);
 
     return results
       .map((res, i) => percentageOfTotal(i, results, results))
-      .map(p => (this.sumOfResultsBalance() / 100) * p);
+      .map((p) => (this.sumOfResultsBalance() / 100) * p);
   }
 
   resultsByStrategyScore() {
@@ -42,18 +43,18 @@ export default class ApprovalVoting {
       .map((choice, i) =>
         this.strategies.map((strategy, sI) =>
           this.votes
-            .map(vote => quadraticMath(i, vote.choice, vote.scores[sI]))
+            .map((vote) => quadraticMath(i, vote.choice, vote.scores[sI]))
             .reduce((a, b: any) => a + b, 0)
         )
       )
-      .map(arr => arr.map(sqrt => [sqrt * sqrt]));
+      .map((arr) => arr.map((sqrt) => [sqrt * sqrt]));
 
     return results.map((res, i) =>
       this.strategies
         .map((strategy, sI) => [
-          percentageOfTotal(0, results[i][sI], results.flat(2))
+          percentageOfTotal(0, results[i][sI], results.flat(2)),
         ])
-        .map(p => [(this.sumOfResultsBalance() / 100) * p])
+        .map((p) => [(this.sumOfResultsBalance() / 100) * p])
     );
   }
 
@@ -76,7 +77,7 @@ export default class ApprovalVoting {
           }% for ${choice}`;
         }
       })
-      .filter(el => el != null)
-      .join(', ');
+      .filter((el) => el != null)
+      .join(", ");
   }
 }
