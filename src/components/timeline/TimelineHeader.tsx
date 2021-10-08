354bb4f2b3360968f9bcb652ae046420c00e37d8
JM Myers <jm.myers@everbridge.com>
2021-10-08 04:16:58 -0400

    Update Timeline

--- src/components/timeline/TimelineHeader.tsx
+++ src/components/timeline/TimelineHeader.tsx
@@ -9,9 +9,10 @@ import ProposalFilters from "../proposal/ProposalFilters";
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 16,
-    paddingTop: 30,
+    paddingTop: 16,
     flexDirection: "row",
     alignItems: "center",
+    height: 35,
   },
   filterContainer: {
     flexDirection: "row",
@@ -24,30 +25,52 @@ const styles = StyleSheet.create({
     fontFamily: "Calibre-Medium",
     marginRight: 6,
   },
+  title: {
+    color: colors.textColor,
+    fontSize: 18,
+    fontFamily: "Calibre-Semibold",
+  },
 });
 
 type TimelineHeaderProps = {
-  filter: { key: string; text: string };
-  setFilter: (filter: { key: string; text: string }) => void;
-  onChangeFilter: (newFilter: string) => void;
+  joinedSpacesFilter: { key: string; text: string };
+  allSpacesFilter: { key: string; text: string };
+  setJoinedSpacesFilter: (filter: { key: string; text: string }) => void;
+  setAllSpacesFilter: (filter: { key: string; text: string }) => void;
+  onChangeJoinedSpacesFilter: (newFilter: string) => void;
+  onChangeAllSpacesFilter: (newFilter: string) => void;
   useFollowedSpaces: boolean;
+  currentIndex: number;
 };
 
 function TimelineHeader({
-  filter,
-  setFilter,
-  onChangeFilter,
-  useFollowedSpaces,
+  onChangeJoinedSpacesFilter,
+  onChangeAllSpacesFilter,
+  setJoinedSpacesFilter,
+  setAllSpacesFilter,
+  joinedSpacesFilter,
+  allSpacesFilter,
+  currentIndex,
 }: TimelineHeaderProps) {
   const { followedSpaces } = useAuthState();
   return (
     <View style={styles.container}>
-      <Text style={common.headerTitle}>{i18n.t("timeline")}</Text>
-      {(followedSpaces.length > 0 || !useFollowedSpaces) && (
+      <Text style={styles.title}>{i18n.t("timeline")}</Text>
+      {currentIndex === 0 ? (
+        followedSpaces.length > 0 ? (
+          <ProposalFilters
+            filter={joinedSpacesFilter}
+            setFilter={setJoinedSpacesFilter}
+            onChangeFilter={onChangeJoinedSpacesFilter}
+          />
+        ) : (
+          <View />
+        )
+      ) : (
         <ProposalFilters
-          filter={filter}
-          setFilter={setFilter}
-          onChangeFilter={onChangeFilter}
+          filter={allSpacesFilter}
+          setFilter={setAllSpacesFilter}
+          onChangeFilter={onChangeAllSpacesFilter}
         />
       )}
     </View>
