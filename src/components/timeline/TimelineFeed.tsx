770666a1b6138f2192cf42bb99d7dc9eb698e620
JM Myers <jm.myers@everbridge.com>
2021-12-07 11:22:15 -0500

    Update space avatar

--- src/components/timeline/TimelineFeed.tsx
+++ src/components/timeline/TimelineFeed.tsx
@@ -137,7 +137,7 @@ function TimelineFeed() {
           }}
         />
       }
-      headerHeight={followedSpaces.length > 0 ? 148 : 50}
+      headerHeight={followedSpaces.length > 0 ? 164 : 50}
       clipHeader
       CollapsibleHeaderComponent={
         <TimelineHeader
