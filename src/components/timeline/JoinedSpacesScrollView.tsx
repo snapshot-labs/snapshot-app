d62b2f1aa65b8773ef02fec5762fdb72a98ccbf2
JM Myers <jm.myers@everbridge.com>
2021-12-27 15:25:55 -0500

    Increment version

--- src/components/timeline/JoinedSpacesScrollView.tsx
+++ src/components/timeline/JoinedSpacesScrollView.tsx
@@ -4,10 +4,17 @@ import { useAuthState } from "context/authContext";
 import SpaceAvatarButton from "components/SpaceAvatarButton";
 import { SPACE_SCREEN } from "constants/navigation";
 import { useNavigation } from "@react-navigation/core";
+import { Fade, Placeholder, PlaceholderMedia } from "rn-placeholder";
 import { useExploreState } from "context/exploreContext";
 import isEmpty from "lodash/isEmpty";
 
-function JoinedSpacesScrollView() {
+interface JoinedSpacesScrollViewProps {
+  useLoader?: boolean;
+}
+
+function JoinedSpacesScrollView({
+  useLoader = false,
+}: JoinedSpacesScrollViewProps) {
   const { colors, followedSpaces } = useAuthState();
   const { spaces } = useExploreState();
   const navigation: any = useNavigation();
@@ -22,28 +29,46 @@ function JoinedSpacesScrollView() {
       }}
     >
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {followedSpaces.map((space, i) => {
-          const spaceDetails = spaces[space?.space?.id];
-          if (isEmpty(spaceDetails)) {
-            return;
-          }
-          return (
-            <SpaceAvatarButton
-              key={i}
-              onPress={() => {
-                navigation.navigate(SPACE_SCREEN, {
-                  space: { id: space?.space?.id, ...spaceDetails },
-                });
-              }}
-              size={64}
-              space={spaceDetails}
-              containerStyle={{
-                marginLeft: 8,
-                marginRight: i === followedSpaces.length - 1 ? 16 : 0,
-              }}
-            />
-          );
-        })}
+        {useLoader
+          ? new Array(3).fill(1).map((v, index: number) => {
+              console.log({ index });
+              return (
+                <Placeholder
+                  key={index}
+                  Animation={Fade}
+                  Left={(props) => (
+                    <PlaceholderMedia
+                      isRound={true}
+                      style={[props.style, { marginLeft: 8 }]}
+                      size={65}
+                    />
+                  )}
+                  style={{ alignItems: "center" }}
+                />
+              );
+            })
+          : followedSpaces.map((space, i) => {
+              const spaceDetails = spaces[space?.space?.id];
+              if (isEmpty(spaceDetails)) {
+                return;
+              }
+              return (
+                <SpaceAvatarButton
+                  key={i}
+                  onPress={() => {
+                    navigation.navigate(SPACE_SCREEN, {
+                      space: { id: space?.space?.id, ...spaceDetails },
+                    });
+                  }}
+                  size={64}
+                  space={spaceDetails}
+                  containerStyle={{
+                    marginLeft: 8,
+                    marginRight: i === followedSpaces.length - 1 ? 16 : 0,
+                  }}
+                />
+              );
+            })}
       </ScrollView>
     </View>
   );
--- src/components/timeline/TimelineHeader.tsx
+++ src/components/timeline/TimelineHeader.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import i18n from "i18n-js";
 import { useAuthState } from "context/authContext";
 import ProposalFilters from "../proposal/ProposalFilters";
 import common from "styles/common";
 import JoinedSpacesScrollView from "components/timeline/JoinedSpacesScrollView";
+
+const styles = StyleSheet.create({
+  proposalFiltersContainer: {
+    paddingRight: 16,
+    paddingTop: 6,
+    paddingBottom: 6,
+    borderBottomWidth: 1,
+  },
+});
 
 interface TimelineHeaderProps {
   joinedSpacesFilter: { key: string; text: string };
@@ -37,19 +46,36 @@ function TimelineHeader({
           {i18n.t("timeline")}
         </Text>
       </View>
-      {!isInitial && (
+      {isInitial ? (
+        <>
+          <JoinedSpacesScrollView useLoader={true} />
+          <View
+            style={[
+              styles.proposalFiltersContainer,
+              {
+                borderBottomColor: colors.borderColor,
+                backgroundColor: colors.bgDefault,
+              },
+            ]}
+          >
+            <ProposalFilters
+              filter={joinedSpacesFilter}
+              showBottomSheetModal={showBottomSheetModal}
+            />
+          </View>
+        </>
+      ) : (
         <>
           {followedSpaces.length > 0 && <JoinedSpacesScrollView />}
           {followedSpaces.length > 0 ? (
             <View
-              style={{
-                paddingRight: 16,
-                paddingTop: 6,
-                paddingBottom: 6,
-                borderBottomWidth: 1,
-                borderBottomColor: colors.borderColor,
-                backgroundColor: colors.bgDefault,
-              }}
+              style={[
+                styles.proposalFiltersContainer,
+                {
+                  borderBottomColor: colors.borderColor,
+                  backgroundColor: colors.bgDefault,
+                },
+              ]}
             >
               <ProposalFilters
                 filter={joinedSpacesFilter}
